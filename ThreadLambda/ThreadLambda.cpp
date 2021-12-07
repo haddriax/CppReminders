4c029a14ae5eced0cdccfd7d6b1ce69b57358124
haddriax <gael-garnier@outlook.fr>
2021-12-07 21:37:18 +0100

    Improve lisibility and change input_data alloc from stack to heap

--- ThreadLambda/ThreadLambda.cpp
+++ ThreadLambda/ThreadLambda.cpp
@@ -2,68 +2,40 @@
 #include <utility>
 #include <vector>
 #include <array>
-#include <thread>
-#include <future>
 #include <random>
 #include <chrono>
+#include <future>
+// #include <thread>
 
-auto Test1(int iterations, const std::function<double()>& chargeAlgorithm) -> long long;
-auto Test2(int iterations, const std::function<double()>& charge_algorithm) -> long long;
+auto test1(int nb_iterations, const std::function<double()>& charge_algorithm) -> long long;
+auto test2(int nb_iterations, const std::function<double()>& charge_algorithm) -> long long;
+
+void run_tests();
+
+// Number of times the charge algorithm is executed, also determine how many Threads are created.
+constexpr int iterations = 256;
+
+//  How heavy the charge algorithm will be.
+constexpr int complexity = 150000;
+
+constexpr int randMin = 1, randMax = 9;
+
 
 int main()
 {
-
-    constexpr int iterations = 10000;
-    constexpr int complexity = 50000;
-
-    constexpr int randMin = 1, randMax = 9;
-
-	// Init rand.
-    std::random_device r;
-    std::uniform_int_distribution<int> uniform_dist(randMin, randMax);
-    std::default_random_engine e1(r());
-
-    std::array<double, complexity> inputDatas {};
-    for (double& data : inputDatas)
-    {
-        data = uniform_dist(e1);
-    }
-
-    // Processed lambda
-    auto process = [complexity, &inData = std::as_const(inputDatas)]()-> double
-    {
-        double val = 0;
-
-        for (int i = 0; i < complexity; ++i)
-            val += sqrt(inData.at(i) * i);
-
-        return val;
-    };
-
-    const long long t1ProcessDuration = Test1(iterations, process);
-    const long long t2ProcessDuration = Test2(iterations, process);
-
-    std::cout << "Process duration using multiple threads : "
-        << t1ProcessDuration
-        << "ms."
-        << std::endl;
-
-    std::cout << "Process duration using one thread : "
-        << t2ProcessDuration
-        << "ms."
-        << std::endl;
+	run_tests();
 }
 
-auto Test1(const int iterations, const std::function<double()>& chargeAlgorithm) -> long long
+auto test1(const int nb_iterations, const std::function<double()>& charge_algorithm) -> long long
 {
     const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();      
 
     std::vector< std::future<double> > threadsReturnValues;
-    threadsReturnValues.reserve(iterations);
+    threadsReturnValues.reserve(nb_iterations);
     
-    for (int x = 0; x < iterations; x++)
+    for (int x = 0; x < nb_iterations; x++)
     {
-        threadsReturnValues.emplace_back(std::async(std::launch::async, chargeAlgorithm));
+        threadsReturnValues.emplace_back(std::async(std::launch::async, charge_algorithm));
     }
 
     for (auto&& x : threadsReturnValues)
@@ -74,12 +46,12 @@ auto Test1(const int iterations, const std::function<double()>& chargeAlgorithm)
     return (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - begin).count());
 }
 
-auto Test2(const int iterations, const std::function<double()>& charge_algorithm) -> long long
+auto test2(const int nb_iterations, const std::function<double()>& charge_algorithm) -> long long
 {
     const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     std::vector<double> returnValues;
-    returnValues.reserve(iterations);
+    returnValues.reserve(nb_iterations);
 
     for (int x = 0; x < iterations; x++)
     {
@@ -87,4 +59,44 @@ auto Test2(const int iterations, const std::function<double()>& charge_algorithm
     }
 
     return (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - begin).count());
+}
+
+void run_tests()
+{
+	auto input_data = std::make_shared<std::array<int, complexity>>();
+
+    std::random_device r;
+    const std::uniform_int_distribution<int> uniform_dist(randMin, randMax);
+    std::default_random_engine e1(r());
+
+    // Fill array with random double values.
+    for (int& data : *(input_data.get()))
+    {
+        data = uniform_dist(e1);
+    }
+
+    // Processed lambda - inData as const should avoid any race condition here.
+    auto process = [inData = std::as_const(input_data)]()-> float
+    {
+        float val = 0;
+
+        for (int i = 0; i < complexity; ++i)
+            val = sqrt(val + static_cast<float>(inData->at(i) * i));
+
+        return val;
+    };
+
+    // Both tests return their execution duration.
+    const long long t1ProcessDuration = test1(iterations, process); // With multi threading.
+    const long long t2ProcessDuration = test2(iterations, process); // On the single main thread
+
+    std::cout << "Process duration using multiple threads : "
+        << t1ProcessDuration
+        << "ms."
+        << std::endl;
+
+    std::cout << "Process duration using one thread : "
+        << t2ProcessDuration
+        << "ms."
+        << std::endl;
 }
